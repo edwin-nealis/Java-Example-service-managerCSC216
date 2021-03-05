2a61149e4abd3c042286f7e56a1b255a9ad7ad20
Edwin Nealis <ewnealis@ncsu.edu>
2021-03-04 20:40:13 -0500

    bug fix

--- Project1/test/edu/ncsu/csc216/service_wolf/model/io/ServiceGroupWriterTest.java
+++ Project1/test/edu/ncsu/csc216/service_wolf/model/io/ServiceGroupWriterTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ServiceGroupsWriterTest {
+public class ServiceGroupWriterTest {
 
 	@Test
 	public void testServiceGroupWriter() {
